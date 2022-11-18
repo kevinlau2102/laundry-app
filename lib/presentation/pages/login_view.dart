c9a0c0635ce090780b453419a5b5575390fb2d17
kevinlau2102 <kevinlaurence49@gmail.com>
2022-11-18 23:38:38 +0700

    Update login_view.dart

--- lib/presentation/pages/login_view.dart
+++ lib/presentation/pages/login_view.dart
@@ -2,13 +2,135 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: ListView(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'images/LogoDraft1_1.png',
+                    fit: BoxFit.fill,
+                    width: 205,
+                  )),
+              const SizedBox(height: 30),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: const Text(
+                  "Login to your account",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF52B2CF),
+                      fontSize: 16),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: SizedBox(
+                  height:50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        labelText: 'Email',
+                        hintText: 'Email'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: SizedBox(
+                  height:50,
+                  child: TextFormField(
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        labelText: "Password",
+                        hintText: "Password",
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                        ),
+                      )),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 165,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor: const MaterialStatePropertyAll<Color>(
+                          Color(0xFFFFA928)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ))),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Column(
+                children: [
+                  const Text(
+                    "Be our laundry customer!",
+                    style: TextStyle(color: Color(0xFF52B2CF), fontSize: 16),
+                  ),
+                  SizedBox(
+                    height:30,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                        onPressed: () {},
+                        child: const Text("Sign up here",
+                            style: TextStyle(
+                                color: Color(0xFFFFA928),fontSize: 16))),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }

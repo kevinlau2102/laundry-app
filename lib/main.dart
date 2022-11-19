4922c09b2a34a3b862757b49e7d940b1d79301df
kevinlau2102 <kevinlaurence49@gmail.com>
2022-11-20 00:00:54 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
+      home: LoginPage(),
     );
   }
 }

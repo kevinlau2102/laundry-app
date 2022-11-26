a1000a61e6c121c24543e16483fe1e7249beb291
kevinlau2102 <kevinlaurence49@gmail.com>
2022-11-26 17:54:30 +0700

    Add GoogleFonts

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: AdsWidget(),
+      home: LoginPage(),
     );
   }
 }

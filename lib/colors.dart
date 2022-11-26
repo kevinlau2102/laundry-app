d5408a320bb9803410315cbebc496f78ebceaade
kevinlau2102 <kevinlaurence49@gmail.com>
2022-11-26 23:38:14 +0700

    Add colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF52B2CF);
+const secondaryColor = Color(0xFF7EC4CF);
+const tertiaryColor = Color(0xFF9CADCE);
+const accentColor = Color(0xFFFFA928);

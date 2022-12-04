4cc008981278fe39327949d099c543cc9bcc0aa1
kevinlau2102 <kevinlaurence49@gmail.com>
2022-12-04 16:41:48 +0700

    Create entities

---
+++ lib/entities/outlets.dart
@@ -0,0 +1,16 @@
+
+import 'package:laundry_app/entities/category.dart';
+
+class Outlets {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final double rating;
+  final String address;
+  final String description;
+  final List<Category> categories;
+
+  Outlets({required this.id,
+  required this.name, required this.imageUrl, required this.rating, required this.address, required this.description, required this.categories });
+  
+}

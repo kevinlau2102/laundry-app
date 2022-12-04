05336c8900a6a4fdfbaa1203c4e2f3d944efb0a7
kevinlau2102 <kevinlaurence49@gmail.com>
2022-12-04 16:25:17 +0700

    Update signup page 2

--- lib/presentation/pages/signup_page_2.dart
+++ lib/presentation/pages/signup_page_2.dart
@@ -1,6 +1,9 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:laundry_app/presentation/pages/login_page.dart';
+import 'package:laundry_app/presentation/pages/pages.dart';
 
 class SignUpPage2 extends StatefulWidget {
   const SignUpPage2({super.key});
@@ -11,7 +14,9 @@ class SignUpPage2 extends StatefulWidget {
 
 class _SignUpPage2State extends State<SignUpPage2> {
   bool _obscureText = true;
+  String? selectedProvinsi;
   List<String> provinsi = [
+    "Select Province",
     "Aceh",
     "Bali",
     "Banten",
@@ -123,23 +128,55 @@ class _SignUpPage2State extends State<SignUpPage2> {
                   padding: const EdgeInsets.symmetric(horizontal: 30),
                   child: SizedBox(
                     height: 50,
-                    child: DropdownButtonFormField(
-                        itemHeight: null,
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton2(
                         isExpanded: true,
-                        decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 5, horizontal: 10),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0)),
-                            labelText: 'Province',
-                            hintText: 'Province'),
-                        items: provinsi.map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value, overflow: TextOverflow.visible),
-                          );
-                        }).toList(),
-                        onChanged: (_) {}),
+                        hint: const Text("Select Province"),
+                        items: provinsi
+                            .map((item) => DropdownMenuItem<String>(
+                              enabled: item != "Select Province",
+                                  value: item,
+                                  child: Text(
+                                    item,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: item == "Select Province" ? Colors.black26 : Colors.black
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                    
+                                  ),
+                                ))
+                            .toList(),
+                        value: selectedProvinsi,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedProvinsi = value as String;
+                          });
+                        },
+                        icon: const Icon(
+                          Icons.keyboard_arrow_down_rounded,
+                        ),
+                        iconSize: 25,
+                        iconEnabledColor: Colors.black45,
+                        iconDisabledColor: Colors.grey,
+                        buttonPadding: const EdgeInsets.only(right: 10),
+                        buttonDecoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.black26,
+                          ),
+                        ),
+                        itemHeight: 40,
+                        itemPadding: const EdgeInsets.symmetric(horizontal: 12),
+                        dropdownMaxHeight: 200,
+                        dropdownDecoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        scrollbarRadius: const Radius.circular(40),
+                        scrollbarThickness: 6,
+                        scrollbarAlwaysShow: true,
+                      ),
+                    ),
                   ),
                 ),
                 const SizedBox(
@@ -160,6 +197,44 @@ class _SignUpPage2State extends State<SignUpPage2> {
                     ),
                   ),
                 ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 165,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).popUntil((route) => route.isFirst);
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: const MaterialStatePropertyAll<Color>(
+                            Color(0xFFFFA928)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ))),
+                    child: const Text(
+                      "Create Account",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15,),
+                SizedBox(
+                  height: 30,
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).popUntil((route) => route.isFirst);
+                      },
+                      child: const Text("Skip for now",
+                          style: TextStyle(
+                              color: Color(0xFFFFA928), fontSize: 16))),
+                )
               ]),
             ],
           )

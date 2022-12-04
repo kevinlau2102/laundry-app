ccff69431f6728bf44d70e11dc98dfd4cd0562a8
kevinlau2102 <kevinlaurence49@gmail.com>
2022-12-04 20:34:30 +0700

    Update search page

---
+++ lib/services/outlets_services.dart
@@ -0,0 +1,41 @@
+import 'package:laundry_app/entities/category.dart';
+import 'package:laundry_app/entities/outlets.dart';
+
+class OutletsServices {
+  List<Outlets> getOutlets() {
+    return [
+      Outlets(
+          id: 1,
+          name: "Laundry Bersih",
+          rating: 4.9,
+          description: "Lorem Ipsum Dolor Sit Amet",
+          imageUrl: "ads2.png",
+          address: "Jl. Surya Sumantri No. 63A Jl. Surya Sumantri No. 63A",
+          categories: [Category("Washing"), Category("Drying"), Category("Ironing")]),
+      Outlets(
+          id: 2,
+          name: "Laundry Bersih",
+          rating: 4.9,
+          description: "Lorem Ipsum Dolor Sit Amet",
+          imageUrl: "ads2.png",
+          address: "Jl. Surya Sumantri No. 63A",
+          categories: [Category("Washing"), Category("Drying"), Category("Ironing")]),
+      Outlets(
+          id: 3,
+          name: "Laundry Bersih",
+          rating: 4.9,
+          description: "Lorem Ipsum Dolor Sit Amet",
+          imageUrl: "ads2.png",
+          address: "Jl. Surya Sumantri No. 63A",
+          categories: [Category("Washing"), Category("Drying"), Category("Ironing")]),
+      Outlets(
+          id: 4,
+          name: "Laundry Bersih",
+          rating: 4.9,
+          description: "Lorem Ipsum Dolor Sit Amet",
+          imageUrl: "ads2.png",
+          address: "Jl. Surya Sumantri No. 63A",
+          categories: [Category("Washing"), Category("Drying"), Category("Ironing")]),
+    ];
+  }
+}
